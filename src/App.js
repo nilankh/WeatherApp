4ba8cac3067c1cef7ebe576173c2e679c7424581
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-01-29 23:08:47 +0530

    calling api from weatherstack

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
   // Statte
   state = {
     coords: {
-      lattitude:45,
-      longitude:60
-    }
-  }
+      lattitude: 45,
+      longitude: 60,
+    },
+  };
 
   // Get device location
   componentDidMount() {
@@ -17,9 +18,15 @@ class App extends Component {
       navigator.geolocation.getCurrentPosition((position) => {
         // console.log(position.coords.longitude);
         let newCoords = {
-          lattitude:position.coords.latitude,
-          longitude:position.coords.longitude
-        }
+          lattitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        };
+        this.setState({ coords: newCoords });
+
+        // Calling API
+        axios.get(
+          `http://api.weatherstack.com/current?access_key=b78694a07f8c247fa13237b76aa51e4e&query=${this.state.coords.latitude},${this.state.coords.longitude}`
+        );
       });
     } else {
       console.log("not supported");
