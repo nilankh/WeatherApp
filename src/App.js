97a28289d59641f6d3919f643a813792cad2c323
NILANK NIKHIL <nilanknikhil@gmail.com>
2021-01-29 22:59:46 +0530

    working on location

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react'
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Weather app</h1>
-    </div>
-  );
+class App extends Component {
+  // Statte
+  state = {
+    coords: {
+      lattitude:45,
+      longitude:60
+    }
+  }
+
+  // Get device location
+  componentDidMount() {
+    if (navigator.geolocation) {
+      // console.log("SUpported");
+      navigator.geolocation.getCurrentPosition((position) => {
+        // console.log(position.coords.longitude);
+        let newCoords = {
+          lattitude:position.coords.latitude,
+          longitude:position.coords.longitude
+        }
+      });
+    } else {
+      console.log("not supported");
+    }
+  }
+
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
